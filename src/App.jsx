b464cb2aadad4402ae643b9f435e331d874545c5
sumudusan <ss.sumuduaberathne@gmail.com>
2025-04-23 23:12:49 +0400

    create AdminHomePage ✅

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './Components/home'
 import { BrowserRouter, Routes , Route } from 'react-router'
 import LoginPage from './pages/loginPage'
 import SignInPage from './pages/signInPage'
+import AdminHomePage from './pages/adminHomePage'
 
 export default function App(){
 
@@ -12,6 +13,8 @@ export default function App(){
      <Routes path="/*">
       <Route path='/login' element={<LoginPage/>}/>
       <Route path='/signup' element={<SignInPage/>}/>
+      <Route path='/admin/*' element={<AdminHomePage/>}/>
+      <Route path='/*' element={<HomePage/>}/>
      </Routes>
     </BrowserRouter>
   </div>
---
+++ src/pages/adminHomePage.jsx
@@ -0,0 +1,26 @@
+import { Link, Route, Routes } from "react-router"
+import { BsBoxSeam, BsCart4, BsGraphUp, BsPerson } from "react-icons/bs"
+
+export default function AdminHomePage(){
+
+    return(
+        <div className="bg-blue-200 w-full h-screen flex">
+            <div className="bg-blue-500 w-[20%] h-screen flex flex-col items-center py-4">
+                <Link className="flex flex-row mb-4 text-white items-center" to ="/admin/dashboard"><BsGraphUp className="mr-2"/> Dashboard</Link>
+                <Link className="flex flex-row mb-4 text-white items-center" to ="/admin/products"><BsBoxSeam className="mr-2"/> Products</Link>
+                <Link className="flex flex-row mb-4 text-white items-center" to ="/admin/orders"><BsCart4 className="mr-2"/> Orders</Link>
+                <Link className="flex flex-row mb-4 text-white items-center" to ="/admin/customers"><BsPerson className="mr-2"/> Customers</Link>
+            </div>
+            
+            <div className="bg-red-600 w-[80%] h-screen">
+              <Routes path="/*">
+               <Route path="/dashboard" element={<h1>Dashboard</h1>}/>
+               <Route path="/products" element={<h1>Products</h1>}/>
+               <Route path="/orders" element={<h1>Orders</h1>}/>
+               <Route path="/customers" element={<h1>Customers</h1>}/>
+               <Route path="/*" element={<h1>404 not found the admin page</h1>}/>
+              </Routes>    
+            </div>
+        </div>
+    )
+}
