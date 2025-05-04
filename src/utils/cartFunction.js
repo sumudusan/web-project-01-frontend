e00027fa8b32f0a2b01384d0a1c8c140b66d4857
sumudusan <ss.sumuduaberathne@gmail.com>
2025-05-04 13:10:27 +0400

    create cartFunction page.✅

---
+++ src/utils/cartFunction.js
@@ -0,0 +1,39 @@
+export function loadCart() {
+    const cart = localStorage.getItem("cart");
+    console.log(localStorage.getItem("cart"));
+  
+    if (cart != null) {
+      return JSON.parse(cart);
+    } else {
+      return [];
+    }
+  }
+  
+  export function addToCart(productId, qty = 1) {
+    if (!Number.isFinite(qty)) {
+      console.error("Invalid quantity:", qty);
+      return;
+    }
+  
+    console.log("Adding to cart:", { productId, qty });
+    const cart = loadCart();
+    console.log("Current cart:", cart);
+  
+    const index = cart.findIndex((item) => {
+      return item.productId == productId;
+    });
+    console.log(index);
+    if (index == -1) {
+      cart.push({ productId, qty });
+    } else {
+      const newQty = cart[index].qty + qty;
+      if (newQty <= 0) {
+        cart.splice(index, 1);
+      } else {
+        cart[index].qty = newQty;
+      }
+    }
+    saveCart(cart);
+    console.log("Updated cart:", cart);
+  }
+  
