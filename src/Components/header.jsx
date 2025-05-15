16a81875ab3bac553b09686f563bd5ed60a41871
sumudusan <ss.sumuduaberathne@gmail.com>
2025-05-15 15:25:28 +0400

    cart and product pages added to homePage.✅

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -4,9 +4,10 @@ export default function Header(){
     return(
         <header className=" w-full h-[100px] bg-white relative flex justify-center items-center">
             <img src="/logo.png" className="h-full rounded-full cursor-pointer absolute left-[10px]"/>
-            <div className="h-full flex items-center w-[500px] justify-between">
+            <div className="h-full flex items-center w-[600px] justify-between">
             <Link to="/" className="font-bold text-xl hover:border-b-2 border-b-amber-400 ">Home</Link>
             <Link to="/products" className="font-bold text-xl hover:border-b-2 border-b-amber-400 ">Products</Link>
+            <Link to="/cart" className="font-bold text-xl hover:border-b-2 border-b-amber-400 ">Cart</Link>
             <Link to="/about" className="font-bold text-xl hover:border-b-2 border-b-amber-400 ">About Us</Link>
             <Link to="/contact" className="font-bold text-xl hover:border-b-2 border-b-amber-400 ">Contact Us</Link>
             </div>
--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router";
 import Header from "../../Components/header";
 import LoginPage from "./loginPage";
 import ProductOverview from "./productOverview";
+import ProductPage from "./product";
+import Cart from "./cart";
 
 export default function HomePage(){
     return(
@@ -10,8 +12,10 @@ export default function HomePage(){
          <div className="w-full h-[calc(100vh-100px)] bg-red-500">
              <Routes path="/*">
             <Route path="/" element={<h1>Home Page</h1>}/>
+            <Route path="/products" element={<ProductPage/>}/>
             <Route path="/login" element={<LoginPage/>}/>
             <Route path="/productinfo/:id" element={<ProductOverview/>}/>
+            <Route path="/cart" element={<Cart/>}/>
             </Routes>
          </div>
             
