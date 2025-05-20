e1d15c6b159bedd4bd8df19a0fc39f1ae9e3756e
sumudusan <ss.sumuduaberathne@gmail.com>
2025-05-20 23:31:13 +0400

    make homePage responsive.👍

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -7,28 +7,17 @@ import ProductPage from "./product";
 import Cart from "./cart";
 import ShippingPage from "./shipping";
 import MyOrdersPage from "./orders";
-import banner from "../../assets/banner.jpg";
+
+import Home from "./home";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen w-full bg-background text-text">
+    <div className=" w-full bg-background text-text">
       <Header />
 
-      <div className="w-full pt-16 px-4">
+      <div className="w-full pt-17 ">
         <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                <img
-                  src={banner}
-                  alt="Banner"
-                  className="w-full max-h-[500px] object-cover rounded-lg mb-8"
-                />
-                
-              </div>
-            }
-          />
+          <Route path="/"element={<Home/>}/>
           <Route path="/products" element={<ProductPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/productinfo/:id" element={<ProductOverview />} />
