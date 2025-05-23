0ebc0d815faa0669ad886a9d25a89ef6f8f03592
sumudusan <ss.sumuduaberathne@gmail.com>
2025-05-23 22:29:54 +0400

    add hover option.👍

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -18,21 +18,23 @@ export default function Header() {
         />
 
         <div className="pl-2">
+          <Link to="/">
           <h1 className="text-heading font-extrabold text-xl font-serif">crystal clear</h1>
           <span className="font-extrabold font-serif">beauty products</span>
+          </Link>
         </div>
 
-        <div className="hidden lg:flex gap-6 justify-center px-17">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/products">Products</NavLink>
-          <NavLink to="/about">About Us</NavLink>
-          <NavLink to="/contact">Contact Us</NavLink>
-          <NavLink to="/cart">Cart</NavLink>
+        <div className="hidden lg:flex gap-6 justify-center px-30 font-bold">
+          <NavLink to="/" className="hover:border-b-2 border-b-primary">Home</NavLink>
+          <NavLink to="/products" className="hover:border-b-2 border-b-primary">Products</NavLink>
+          <NavLink to="/about" className="hover:border-b-2 border-b-primary">About Us</NavLink>
+          <NavLink to="/contact" className="hover:border-b-2 border-b-primary">Contact Us</NavLink>
+          <NavLink to="/cart" className="hover:border-b-2 border-b-primary">Cart</NavLink>
         </div>
 
-        <div className="hidden lg:block pl-39">
+        <div className="hidden lg:block pl-38">
           <Link to="/login">
-            <button className="px-6 py-2 font-semibold bg-accent text-white rounded-lg shadow hover:bg-[#d88c8c] transition">
+            <button className="px-6 py-2 font-semibold bg-accent text-white rounded-lg shadow hover:bg-primary transition">
               Login
             </button>
           </Link>
